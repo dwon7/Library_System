f7ab87075d891d197956e72035c6fd54713f2496
khiemnv <khiem.nguyen@vconnex.vn>
2026-06-27 07:00:58 +0700

    update warning

--- LibraryApp/lib/features/inventory_ledger_detail/inventory_ledger_detail_provider.dart
+++ LibraryApp/lib/features/inventory_ledger_detail/inventory_ledger_detail_provider.dart
@@ -30,4 +30,17 @@ class InventoryLedgerDetailProvider {
       return null;
     }
   }
+
+  // TODO: getBooksByIds | Input: List<String> bookIds | Output: List<BookDetailRes>? | Lấy chi tiết 1 sách theo mã. null nếu không thấy
+  Future<List<BookDetailRes>?> getBooksByIds(List<String> bookIds) async {
+    await Future.delayed(const Duration(milliseconds: 500));
+
+    try {
+      return _storageService.books.where(
+            (book) => bookIds.contains(book.bookId),
+      ).toList();
+    } catch (_) {
+      return null;
+    }
+  }
 }
